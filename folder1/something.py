738b0118659f39a1708337f7cd791ea1444572f8
Nick Lehman <nicholas.lehman@simplybusiness.com>
2021-01-19 20:42:56 +0000

    setec astronomy

--- folder1/something.py
+++ folder1/something.py
@@ -4,7 +4,7 @@ import random
 
 token="xoxb-xxxxxxxxx-xxxx"
 hook_url="https://hooks.slack.com/services/T012DKAL2212/BUMMER21X2/askEKERO323kKdA"
-AWS_KEY="AKIAIOSFODNN7EXAMPLE"
+AWS_KEY="AKIAIEXAMPLE"
 AWS_SECRET="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 Password="thisisapassword12#!@"
 
--- something.py
+++ something.py
@@ -2,9 +2,9 @@
 import requests
 import random
 
-token="xoxb-xxxxxxxxx-xxxx"
-hook_url="https://hooks.slack.com/services/T012DKAL2212/BUMMER21X2/askEKERO323kKdA"
-AWS_KEY="AKIAIOSFODNN7EXAMPLE"
-AWS_SECRET="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-Password="thisisapassword12#!@"
-
+token="xoxb-xxxdddxxxxx-xxxx"
+hook_url="https://hooks.slack.com/services/TIKAL2000/METH0DM4N/askEKERO323kKdA"
+AWS_KEY="AKIAIFAKEFAKEFAKE0EXAMPLE"
+AWS_SECRET="wJalrXUtnFEMI/K7MDENG/aFTeRfiNDEXAMPLEKEY"
+Password="thisisapassword23#"
+token2="xapp-1-AUU8W3P2R-8675309JENNY-6a4cf2470e71e1c2fe42cf8dbc7ad94c66675a5e658547e825904dbcc9ac3438"
